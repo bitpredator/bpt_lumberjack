f3036fd422fc788d1462a601be79e65f6869bdbc
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-02-28 17:47:13 +0100

    refactor(bpt_taglialegna)
    an ideal wood collection system for crafting

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,14 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
 
-description 'bpt_taglialegna'
+lua54 'yes'
+
 author 'bitpredator'
 
-version '0.0.2'
+version '0.0.4'
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
+	'@oxmysql/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'locales/*.lua',
 	'config.lua',
